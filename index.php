fa294e89b4e16e6ebd546a09e4134b42a5a88e7b
unkaer <ababwbq@outlook.com>
2020-10-28 14:12:38 +0800

    精简

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-
    <form action="" method='POST'>
       <p>单词查询：<input name="word" style="" autofocus value="">
       <input type="submit" value="查询"></p>
@@ -17,80 +16,104 @@
    }
    $db = new MyDB();
    
-   $list0=array("a","b","c","def","ghijk","lmno","pqr","s","tuvwxyz");  // 0-8
-   $list1=array("dict_a_b","dict_bcz","dict_c","dict_d_f","dict_g_k","dict_l_o","dict_p_r","dict_s","dict_t_z");
-   $list2=array("variant_a_b","variant_a_b","variant_c","variant_d_f","variant_g_k","variant_l_o","variant_p_r","variant_s","variant_t_z");
+   $N=8;
+   $list0=array("dict_a_b","dict_c","dict_d_f","dict_g_k","dict_l_o","dict_p_r","dict_s","dict_t_z");
+   $list1=array("variant_a_b","variant_a_b","variant_c","variant_d_f","variant_g_k","variant_l_o","variant_p_r","variant_s","variant_t_z");
    
    if(array_key_exists("word", $_POST)|array_key_exists("word", $_GET)){
       if(isset($_POST["word"])){$word = $_POST["word"];}else{$word = $_GET["word"];}
+      $word=trim($word);
+      $word=strtolower($word);
       if($word==""){
          print_r("请输入要查询的单词");
-      }else{
+      }
+      else{    // 查询功能
          $fist=substr($word, 0 , 1);
-         for($i=0;$i<9;$i++){   // 判断第一个字母，确定数据库
-            if(strpos($list0[$i],$fist) !== false){ 
+         if(($fist>="a"&&$fist<="z")|($fist>="A"&&$fist<="Z")){   // 是字母，找单词
+            for($i=0;$i<$N;$i++){   // 每一个都跑一遍
                $sql =<<<EOF
-               SELECT * from $list1[$i] WHERE word=="$word";
-            EOF;
-            break;
+                  SELECT * from $list0[$i] WHERE word=="$word";
+               EOF;
+               $ret = $db->query($sql);
+               while($row = $ret->fetchArray(SQLITE3_ASSOC) ){
+                  echo "<br>单词：<a href=\"?word=".$row['word']."\">".$row['word']."</a>\n<br>";
+                  echo "发音：". $row['accent'] . "\n<br>";
+                  echo "中文意思：". $row['mean_cn'] . "\n<br>";
+                  echo "数据库：". $list0[$i] . "\n<br>";
+               }
             }
-         }
-      
-         $ret = $db->query($sql);
-         while($row = $ret->fetchArray(SQLITE3_ASSOC) ){
-            echo "单词：". $row['word'] . "\n<br>";
-            echo "发音：". $row['accent'] . "\n<br>";
-            echo "中文意思：". $row['mean_cn'] . "\n<br>";
-         }
-      
-         for($i=0;$i<9;$i++){
-            if(strpos($list0[$i],$fist) !== false){ 
+
+            //是否存在变体
+            $j=0;
+            for($i=0;$i<$N;$i++){  // 每一个都跑一遍
                $sql =<<<EOF
-               SELECT * from $list2[$i] WHERE variant=="$word" OR origin=="$word";
-            EOF;
-            $j=$i;
-            break;
+                  SELECT * from $list1[$i] WHERE variant=="$word" OR origin=="$word";
+               EOF;
+               $ret = $db->query($sql);
+               while($row = $ret->fetchArray(SQLITE3_ASSOC) ){
+                  $origin[$j]=$row["origin"];
+                  $variant[$j]=$row["variant"];
+                  $j++;
+               }
+            }
+      
+            if(isset($variant)){
+               print_r("原型--->变体<br>");
+               for($i=0;$i<sizeof($variant);$i++){
+                  print_r('<a href="./?word='.$origin[$i].'">'.$origin[$i].'</a>--->');
+                  print_r('<a href="./?word='.$variant[$i].'">'.$variant[$i].'</a><br>');
+               }
             }
          }
-      
-      
-         $i=0;
-         $ret = $db->query($sql);
-         while($row = $ret->fetchArray(SQLITE3_ASSOC) ){
-            $origin=$row["origin"];
-            $variant[$i]=$row["variant"];
-            $i++;
-         }
-         if($variant[0]==$word){   // 查找所有变型
-            $sql =<<<EOF
-               SELECT * from $list2[$j] WHERE origin=="$origin";
-            EOF;
-            $i=0;
-            $ret = $db->query($sql);
-            while($row = $ret->fetchArray(SQLITE3_ASSOC) ){
-               $variant[$i]=$row["variant"];
-               $i++;
+         else{   // 非字母，查找中文
+            for($i=0;$i<$N;$i++){   // 每一个都跑一遍
+               $sql =<<<EOF
+                  SELECT * from $list0[$i] WHERE mean_cn LIKE "%$word%";
+               EOF;
+               $ret = $db->query($sql);
+               while($row = $ret->fetchArray(SQLITE3_ASSOC) ){
+                  echo "单词：<a href=\"?word=".$row['word']."\">".$row['word']."</a>\n<br>";
+                  echo "发音：". $row['accent'] . "\n<br>";
+                  echo "中文意思：". $row['mean_cn'] . "\n<br>";
+               }
             }
-         }
-      
-         print_r('原型为:<a href="./?word='.$origin.'">'.$origin.'</a>');
-         print_r("<br>变型有: ");
-         for($i=0;$i<sizeof($variant);$i++){
-            print_r('<a href="./?word='.$variant[$i].'">'.$variant[$i].'</a>;');
          }
       }
-   }else{
+   }
+   else{
       echo "随机展示: <br>";
       $i = rand(0,8);
       $sql =<<<EOF
-         SELECT * FROM $list1[$i] ORDER BY RANDOM() limit 1
+         SELECT * FROM $list0[$i] ORDER BY RANDOM() limit 1
       EOF;
       $ret = $db->query($sql);
       while($row = $ret->fetchArray(SQLITE3_ASSOC) ){
+         $word=$row['word'];
          echo "单词：". $row['word'] . "\n<br>";
          echo "发音：". $row['accent'] . "\n<br>";
          echo "中文意思：". $row['mean_cn'] . "\n<br>";
+      }
+      //是否存在变体
+      $j=0;
+      for($i=0;$i<$N;$i++){  // 每一个都跑一遍
+         $sql =<<<EOF
+            SELECT * from $list1[$i] WHERE variant=="$word" OR origin=="$word";
+         EOF;
+         $ret = $db->query($sql);
+         while($row = $ret->fetchArray(SQLITE3_ASSOC) ){
+            $origin[$j]=$row["origin"];
+            $variant[$j]=$row["variant"];
+            $j++;
          }
+      }
+
+      if(isset($variant)){
+         print_r("原型--->变体<br>");
+         for($i=0;$i<sizeof($variant);$i++){
+            print_r('<a href="./?word='.$origin[$i].'">'.$origin[$i].'</a>--->');
+            print_r('<a href="./?word='.$variant[$i].'">'.$variant[$i].'</a><br>');
+         }
+      }
    }
    $db->close();
 
